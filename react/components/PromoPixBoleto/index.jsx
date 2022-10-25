fada466f6043b1a97626c85c8c06b8150949a1f1
camatta <contatogcamatta@gmail.com>
2022-10-25 11:11:48 -0300

    melhorias reuniao v2

--- react/components/PromoPixBoleto/index.jsx
+++ react/components/PromoPixBoleto/index.jsx
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 import useProduct from 'vtex.product-context/useProduct'
 
 import { FormattedCurrency } from 'vtex.format-currency'
@@ -11,12 +9,6 @@ const PromoPixBoleto = () => {
   const productContext = useProduct ()
   // console.log('productContext', productContext)
 
-  axios.get('/api/rnb/pvt/calculatorconfiguration/0031814d-3099-4cf0-a0bf-eda026ecf232')
-  .then(function (response) {
-    const percentualDesconto = (response.data.percentualDiscountValue);
-    console.log(percentualDesconto);
-  })
-  
 
   const {product} = useProduct ( )
   const valorDesconto = (product?.priceRange.sellingPrice.highPrice) / 100 * 10;
