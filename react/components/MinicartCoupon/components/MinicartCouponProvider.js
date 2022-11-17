c7229f1475b35eab66b1779f7729d2d17406f760
camatta <contatogcamatta@gmail.com>
2022-11-17 11:43:28 -0300

    tratativa cupom 3

--- react/components/MinicartCoupon/components/MinicartCouponProvider.js
+++ react/components/MinicartCoupon/components/MinicartCouponProvider.js
@@ -8,6 +8,7 @@ const MinicartCouponContext = createContext();
 const MinicartCouponProvider = ({ children }) => {
     const [state, setState] = useState({
         coupon: '',
+        discountValue: 0,
         loading: false,
         codeReturn: {
             success: false,
@@ -33,6 +34,7 @@ const MinicartCouponProvider = ({ children }) => {
             return setState({
                 loading: false,
                 coupon: '',
+                discountValue: 0,
                 codeReturn: {
                     success: false,
                     error: true,
@@ -55,6 +57,7 @@ const MinicartCouponProvider = ({ children }) => {
                     setState({
                         loading: false,
                         coupon: '',
+                        discountValue: 0,
                         codeReturn: {
                             success: false,
                             error: true,
@@ -65,7 +68,7 @@ const MinicartCouponProvider = ({ children }) => {
                 if (marketingData.coupon === coupon) {
                     let discountValueToShow = 0;
 
-                    if(typeof totalizers[1].value === 'number') {
+                    if(totalizers[1].value && typeof totalizers[1].value === 'number') {
                         discountValueToShow = totalizers[1].value;
                     }
 
